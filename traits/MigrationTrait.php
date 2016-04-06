41e1075a73472cecbf8c852618af305632bf7f62
fps01 <fps.06@mail.ru>
2016-04-06 19:34:12 +0300

    added MigrationTrait

---
+++ traits/MigrationTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @copyright Copyright (c) 2015 Yiister
+ * @license https://github.com/yiister/yii2-advanced-db/blob/master/LICENSE
+ * @link https://github.com/yiister/yii2-advanced-db
+ */
+
+namespace yiister\db\traits;
+
+use yiister\db\helpers\DbHelper;
+
+trait MigrationTrait
+{
+    /**
+     * Builds a SQL statement for adding a foreign key constraint to an existing table.
+     * The method will properly quote the table and column names.
+     * @param string $name the name of the foreign key constraint.
+     * @param string $table the table that the foreign key constraint will be added to.
+     * @param string|array $columns the name of the column to that the constraint will be added on. If there are multiple columns, separate them with commas or use an array.
+     * @param string $refTable the table that the foreign key references to.
+     * @param string|array $refColumns the name of the column that the foreign key references to. If there are multiple columns, separate them with commas or use an array.
+     * @param string $delete the ON DELETE option. Most DBMS support these options: RESTRICT, CASCADE, NO ACTION, SET DEFAULT, SET NULL
+     * @param string $update the ON UPDATE option. Most DBMS support these options: RESTRICT, CASCADE, NO ACTION, SET DEFAULT, SET NULL
+     */
+    public function addForeignKey($name, $table, $columns, $refTable, $refColumns, $delete = null, $update = null)
+    {
+        if ($name === null) {
+            DbHelper::buildForeignKey($table, $columns, $refTable, $refColumns, $this->db);
+        }
+        parent::addForeignKey($name, $table, $columns, $refTable, $refColumns, $delete, $update);
+    }
+
+    /**
+     * Builds and executes a SQL statement for creating a new index.
+     * @param string $name the name of the index. The name will be properly quoted by the method.
+     * @param string $table the table that the new index will be created for. The table name will be properly quoted by the method.
+     * @param string|array $columns the column(s) that should be included in the index. If there are multiple columns, please separate them
+     * by commas or use an array. Each column name will be properly quoted by the method. Quoting will be skipped for column names that
+     * include a left parenthesis "(".
+     * @param boolean $unique whether to add UNIQUE constraint on the created index.
+     */
+    public function createIndex($name, $table, $columns, $unique = false)
+    {
+        if ($name === null) {
+            $name = DbHelper::buildIndexKey($table, $columns, $unique, $this->db);
+        }
+        parent::createIndex($name, $table, $columns, $unique);
+    }
+}
